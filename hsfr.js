245c37d06e88d383a0871bf1293edf0e03ff1477
stefegg <38544233+stefegg@users.noreply.github.com>
2018-05-19 16:29:28 -0400

    updates
    updates

--- hsfr.js
+++ hsfr.js
@@ -1,10 +1,12 @@
-$("#pay-btn").click(function() {
+$("#pay-btn").click(function(){
 var donation = $("#donate-amount").val();
 if (donation == 1000000) {
 $("#display-result").text("output 1");
 } else if (donation == 100000){
-$("#display-result").text("output 2").css("color", "red");
+$("#display-result").text("Getting warmer... but did you really think this was enough?").css("color", "red");
 } else if (donation == 10000){
-$("#display-result").text("output 3");
+$("#display-result").text("That's very cute, but please select a real amount");
+} else {
+$("#display-result").text("Please select an amount.");
 }
 });
